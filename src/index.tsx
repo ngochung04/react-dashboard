9347275a265f323e3f682ed8de2bc50379e34a1e
hungdn <hungdn@runsystem.net>
2023-10-06 15:37:11 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,33 @@
-import { ColorModeScript } from "@chakra-ui/react"
-import * as React from "react"
-import * as ReactDOM from "react-dom/client"
-import { App } from "./App"
-import reportWebVitals from "./reportWebVitals"
-import * as serviceWorker from "./serviceWorker"
+import {
+  Box,
+  ChakraProvider,
+  ColorModeScript,
+  Grid,
+  theme,
+} from "@chakra-ui/react";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import * as serviceWorker from "./serviceWorker";
+import { ColorModeSwitcher } from "components/ColorModeSwitcher";
 
-
-const container = document.getElementById("root")
-if (!container) throw new Error('Failed to find the root element');
-const root = ReactDOM.createRoot(container)
+const container = document.getElementById("root");
+if (!container) throw new Error("Failed to find the root element");
+const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
     <ColorModeScript />
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister()
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
-
+serviceWorker.unregister();
