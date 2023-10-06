fd5ff4081a5e179edc3df92eb7b8f299629c6a8b
hungdn <hungdn@runsystem.net>
2023-10-06 17:09:56 +0700

    update

--- src/pages/default/index.tsx
+++ src/pages/default/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Grid } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
 import { ColorModeSwitcher } from "components/ColorModeSwitcher";
 
